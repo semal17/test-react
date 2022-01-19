06a55e9b19fc461a7ed7cf3069162f6e6d174aeb
Semen <senia-2010@mail.ru>
2022-01-19 21:32:11 +0300

    add new element

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
 import './App.css';
 import React, { useState } from 'react';
-import goodsArr from './goods.json';
-import Goods from './Goods';
-import Cart from './Cart';
+
 
 function App() {
-  const [cart, setCart] = useState({});
-  const [count, setCount] = useState(0);
+  const h1 = React.createElement('h1', {}, 'unit08');
+  const h2 = React.createElement('h2', { 'className': 'text-orange' }, 'header 2');
+  const p = React.createElement('p', { 'style': { 'color': 'red' } }, 'this is p');
+  const input = React.createElement('input', { 'value': '55' });
+  const p1 = React.createElement('p', {}, 'hi');
+  const p2 = React.createElement('p', {}, 'world');
+  const div = React.createElement('div', { 'className': 'text-grey' }, p1, p2);
 
 
-  let addToCart = (event) => {
-    event.preventDefault(); 
-    if (!event.target.classList.contains('add-to-cart')) return false;
-    let cartTemp = cart;
-    cartTemp[event.target.dataset.key] ? cartTemp[event.target.dataset.key]++ : cartTemp[event.target.dataset.key] = 1;
-    // cartTemp++;
-    setCart(cartTemp);
-    let count1 = count; 
-    count1++;
-    setCount(count1);
-}
+  let rin = React.createRef();
+  const [li, setLi] = useState();
+  let checkIn = () => {
+    if (rin.current.value !== '') {
+      setLi(React.createElement('li', {}, rin.current.value));
+    }
+  };
 
-let showCart;
-if (count !== 0) {
-    showCart =  <Cart cart={cart} goods={goodsArr}/>;
-}
-else {
-    showCart = 'Empty';
-}
 
   return (
-    <div className="container">
-      <h1>Cart</h1>
-      <div className="goods-field" onClick={addToCart}>
-        {goodsArr.map(item => <Goods title={item.title} cost={item.cost} image={item.image} articul={item.articul} key={item.articul} />)}
-      </div>
-      {showCart}
-    </div>
+    <>
+      {h1}
+      {h2}
+      {p}
+      {div}
+      {input}
+      <input ref={rin}></input>
+      <button onClick={checkIn}>Button</button>
+      <ul>{li}</ul>
+    </>
   )
 }
 
