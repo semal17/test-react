a9d83133c49230016943f768d01c0b2154821e19
Semen <senia-2010@mail.ru>
2022-01-24 15:36:33 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import './App.css';
 import React from 'react';
-// import Button from './Button';
-import PlaceholderPost from './PlaceholderPost';
-import PlaceholderPostHook from './PlaceholderPostHook'
-import Comment from './Comment';
-import CommentHook from './CommentHook';
-import Comment2 from './Comment2';
-import Comment2Hook from './Comment2Hook';
-
+import In from './In';
+import Out from './Out'
+import './App.css';
 
 function App() {
   return (
-    <>
-      <PlaceholderPostHook />
-      <CommentHook />
-      <Comment2Hook />
-      {/* <PlaceholderPost /> */}
-      {/* <Comment/> */}
-      {/* <Comment2/> */}
-    </>
+    <div className="App">
+      <In />
+      <Out />
+    </div>
   );
 }
 
